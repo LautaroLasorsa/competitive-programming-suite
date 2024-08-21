8bfe6c5d3a4d9fc133c11f72ed6bd15a9e63a90c
Lautaro Lasorsa <llasorsa@alumno.unlam.edu.ar>
2024-08-21 02:16:56 -0300

    Update cp-get-notebook.sh
    Se agrego el notebook python-unlam.
    
    Además se agregó como posible el argumento all que permite descargar todos los notebooks disponibles.

--- scripts/cp-get-notebook.sh
+++ scripts/cp-get-notebook.sh
@@ -1,13 +1,39 @@
 
 cd "$CP"
-if [ "$1" = "vasito" ]; then
+algun_notebook=false
+
+if [ "$1" = "vasito" ] || [ "$1" = "all" ]; then
     if [ ! -d "icpc-team-notebook-el-vasito" ]; then
         git clone https://github.com/mhunicken/icpc-team-notebook-el-vasito.git
     fi 
-    cd icpc-team-notebook-el-vasito/
+    
+    if [ "$1" != "all" ];then
+        cd icpc-team-notebook-el-vasito/
+        code .
+    fi
+    algun_notebook=true
+fi
+
+if [ "$1" = "python-unlam" ] || [ "$1" = "all" ]; then
+    if [ ! -d "notebook-unlam-python" ]; then
+        git clone https://github.com/LautaroLasorsa/notebook-unlam-python.git
+    fi
+    if [ "$1" != "all" ];then
+        cd notebook-unlam-python/
+        code .
+    algun_notebook=true
+    fi
+fi
+
+if [ "$1" = "all" ]; then
+    echo "Todos los notebooks han sido descargados."
     code .
-else
+fi
+
+if [ "$algun_notebook" = false ]; then
     echo "Listas de notebooks disponibles:
+        all: Descarga todos los notebooks disponibles.
         vasito : Notebook del equipo de ICPC GGDem de la Universidad Nacional de Córdoba (UNC)
+        python-unlam : Notebook realizado para los equipos formados en el curso ICPC UNLaM (Universidad Nacional de La Matanza) que decidieron utilizar Python.
     "
 fi
